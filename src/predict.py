133dfe562dcc23c4956a43ad47d776939ea51f38
rameshpawa <your.email@example.com>
2026-02-24 23:53:03 +0530

    yaml file changed-4 for testing

--- src/predict.py
+++ src/predict.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 def predict(input_data):
     model = joblib.load("models/model.pkl")
+    if isinstance(input_data, list):
+        input_data = [input_data]
+
+    df = pd.DataFrame(input_data)
     return model.predict(pd.DataFrame(input_data))
