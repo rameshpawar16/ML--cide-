bc28d6b8a78ff8ff916001b8b52492ed72ff4089
rameshpawa <your.email@example.com>
2026-02-24 23:56:12 +0530

    yaml file changed-5 for testing

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -3,4 +3,4 @@ from src.predict import predict
 def test_predict():
     sample = [5.1,3.5,1.4,.2]
     result = predict(sample)
-    assert len(result) == "Setosa"
+    assert result[0] == ["Setosa", "Versicolor", "Virginica"]
